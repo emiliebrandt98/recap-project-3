e960f5ba126e25b2e6b1f8b8f0417f1d18f37c35
Emilie Brandt <emiliebrandt@gmx.de>
2026-07-28 10:54:41 +0200

    refactoring navpagination

--- components/NavPagination/NavPagination.js
+++ components/NavPagination/NavPagination.js
@@ -1,1 +1,10 @@
+export function NavPagination(textContent) {
+  // create Elements
+  const span = document.createElement("span");
 
+  // Element content
+  span.classList.add("navigation__pagination");
+  span.textContent = textContent;
+
+  return span;
+}
